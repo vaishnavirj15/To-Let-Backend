4ac06bf5446c23fe9d4adb2c0b45b6cd92354cd4
vaishnavirj15 <rajvaishnavi475@gmail.com>
2024-08-05 12:09:37 +0530

    Authentication added

---
+++ routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  login,
+  forgotPassword,
+  resetPassword,
+  register,
+} = require("../controllers/authController");
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+
+module.exports = router;
