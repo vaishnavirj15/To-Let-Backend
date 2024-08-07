d944ce3d02adc683ef5bd15817f7a1a9acea2c94
Madhuri Tugave <[dalvemadhuri974@gmail.com]>
2024-08-07 16:03:53 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
+
+
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGO_URI}`
+      `${process.env.MONGODB_URI}`
     );
     console.log(
       `\n MongoDB conncted !! DB HOST: ${connectionInstance.connection.host}`
